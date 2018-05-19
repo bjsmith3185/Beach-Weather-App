69b77accd52a1078ef5d7ff63e240ea15386c2f5
bjsmith3185 <bjsmith3185@hotmail.com>
2018-05-19 12:26:10 -0400

    completed picture carousel, changed navbar

---
+++ assets/javascript/picture_carousel.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    $('#beach-carousel').hide();
+    $('#golf-carousel').hide();
+    $('#shopping-carousel').hide();
+    $('#eatting-carousel').hide();
+     
+     
+     $("#submit").on("click", function() {
+   //-------- code to hide the search container -----  
+      $("#search-container").hide();
+   
+       
+ //-------- var for search drop down -------
+ 
+        var search = $("#carousel-search").val();
+        console.log("this is search: " + search);
+       
+        if (search === "beach-carousel") {
+            $("#beach-carousel").show();
+        } else if (search === "shopping-carousel") {
+            $("#shopping-carousel").show();
+        } else if (search === "golf-carousel") {
+            $("#golf-carousel").show();
+        } else if (search === "eatting-carousel") {
+            $("#eatting-carousel").show();
+        };
+
+     }); // end of the onclick function "submit"
+ 
+ //   // ------------Reset Button --------
+   
+     $("#reset-button-city-search").on("click", function() {
+   
+         $("#beach-carousel").hide();
+         $("#shopping-carousel").hide();
+         $("#golf-carousel").hide();
+         $("#eatting-carousel").hide();
+         $("#search-container").show();
+         
+  
+     });
+      
+   }); // end of document.ready
+   
+   
+ 
+ 
+ // put the search col-xs-4 inside a new container and give it CSS. 
+ // rename the #jumbo container so it doesnt take up the entire 12 cols 
+   
